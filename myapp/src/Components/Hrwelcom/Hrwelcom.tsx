9640b475113403b7668c0c9494c7f697c82f1a25
Damilola Sanni <sannifortune11@gmail.com>
2023-06-24 20:25:12 +0100

    auth

--- myapp/src/Components/Hrwelcom/Hrwelcom.tsx
+++ myapp/src/Components/Hrwelcom/Hrwelcom.tsx
@@ -1,212 +1,224 @@
-import React from 'react'
-import styled from 'styled-components'
-import img from "../Assets/slide1.svg"
-import img2 from "../Assets/vice.jpeg"
-import {AiOutlineArrowRight} from "react-icons/ai"
-import { NavLink } from 'react-router-dom'
+import React from "react";
+import styled from "styled-components";
+import img from "../Assets/slide1.svg";
+import img2 from "../Assets/vice.jpeg";
+import { AiOutlineArrowRight } from "react-icons/ai";
+import { NavLink } from "react-router-dom";
 
 const Hrwelcome = () => {
   return (
     <Container>
-        <Top><h1>Vice Chancellor's Welcome Address</h1></Top>
-        <br />
-        <br />
-        <Wrapper>
-            <Right>
-                <Card src={img2}/>
-                <Hold>
-                        <Up>Professor Abdul-Rasheed Na'Allah</Up>
-                        <Down>Vice Chancellor, University of Abuja</Down>
-                </Hold>
-            </Right>
-            <Left>
-                {/* <Icon src={img} /> */}
-                <P><p>It is my pleasure to welcome you all to the Centre for Undergraduate Research 
-                    at the University of Abuja. As the Vice Chancellor of this esteemed institution, 
-                    which is designed to serve as a hub for the academic exploration of our undergraduate students.
-                    The Centre for Undergraduate Research is a long-awaited and......</p></P>
-                <NavLink to="/about" style={({ isActive }) => ({
-    textDecoration: "none",
-    color: isActive ? '#219653' : '#5f5f5f'
-  })}>
-                    <G>Read more
-                    <Icons>
-                        <AiOutlineArrowRight />
-                    </Icons>
-                </G>
-                </NavLink>
-            </Left>
-        </Wrapper>
+      <Top>
+        <h1>Vice Chancellor's Welcome Address</h1>
+      </Top>
+      <br />
+      <br />
+      <Wrapper>
+        <Right>
+          <Card src={img2} />
+          <Hold>
+            <Up>Professor Abdul-Rasheed Na'Allah</Up>
+            <Down>Vice Chancellor, University of Abuja</Down>
+          </Hold>
+        </Right>
+        <Left>
+          {/* <Icon src={img} /> */}
+          <P>
+            <p>
+              It is my pleasure to welcome you all to the Centre for
+              Undergraduate Research at the University of Abuja. As the Vice
+              Chancellor of this esteemed institution, which is designed to
+              serve as a hub for the academic exploration of our undergraduate
+              students. The Centre for Undergraduate Research is a long-awaited
+              and......
+            </p>
+          </P>
+          <NavLink
+            to="/about"
+            style={({ isActive }) => ({
+              textDecoration: "none",
+              color: isActive ? "#219653" : "#5f5f5f",
+            })}
+          >
+            <G>
+              Read more
+              <Icons>
+                <AiOutlineArrowRight />
+              </Icons>
+            </G>
+          </NavLink>
+        </Left>
+      </Wrapper>
     </Container>
-  )
-}
+  );
+};
 
-export default Hrwelcome
+export default Hrwelcome;
 const Top = styled.div`
-    h1{
-        font-size: 32px;
-        color: #00A85A;
-        @media screen and (max-width: 768px) {
-        width: 100%;
-        text-align: center;
-        font-size: 30px;
+  h1 {
+    font-size: 32px;
+    color: #00a85a;
+    @media screen and (max-width: 768px) {
+      width: 100%;
+      text-align: center;
+      font-size: 30px;
     }
     @media screen and (max-width: 425px) {
-        width: 100%;
-        text-align: center;
-        font-size: 25px;
+      width: 100%;
+      text-align: center;
+      font-size: 25px;
     }
-}
-`
+  }
+`;
 const Down = styled.div`
-    font-size: .9375rem;
-    font-size: .9375rem;
-    margin-top: 10px;
-    color: #E5F6EE;
-    text-align: center;
-`
+  font-size: 0.9375rem;
+  font-size: 0.9375rem;
+  margin-top: 10px;
+  color: #e5f6ee;
+  text-align: center;
+`;
 const Hol = styled.div`
-    width: 120px;
-    display: flex;
-    justify-content: center;
-    flex-direction: column;
-    align-items: center
-`
+  width: 120px;
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
+  align-items: center;
+`;
 const Up = styled.div`
-    color: white;
-    font-size: 24px;
-    line-height: 2.5rem;
-    font-weight: 600;
-    text-align: center;
-`
+  color: white;
+  font-size: 24px;
+  line-height: 2.5rem;
+  font-weight: 600;
+  text-align: center;
+`;
 const Hold = styled.div`
-    width: 450px;
-    height: 160px;
-    justify-content: center;
-    background-color: #10A662;
-    position: absolute;
-    border-radius: 10px;
-    left: 295px;
-    flex-direction: column;
-    top: 320px;
-    display: flex;
-    align-items: center;
-    @media screen and (max-width: 1024px) {
-        width: 100%;
-        left: 0;
-        right: 0;
-        margin-top: 80px;
-    }
-    @media screen and (max-width: 425px) {
-        width: 100%;
-        left: 0;
-        right: 0;
-        margin-top: -30px;
-    }
-    /* justify-content: space-around; */
-`
+  width: 450px;
+  height: 160px;
+  justify-content: center;
+  background-color: #10a662;
+  position: absolute;
+  border-radius: 10px;
+  left: 295px;
+  flex-direction: column;
+  top: 320px;
+  display: flex;
+  align-items: center;
+  @media screen and (max-width: 1024px) {
+    width: 100%;
+    left: 0;
+    right: 0;
+    margin-top: 80px;
+  }
+  @media screen and (max-width: 425px) {
+    width: 100%;
+    left: 0;
+    right: 0;
+    margin-top: -30px;
+  }
+  /* justify-content: space-around; */
+`;
 const Card = styled.img`
-    width: 400px;
-    height: 410px;
-    border-radius: 10px;
-    object-fit: cover;
-    @media screen and (max-width: 425px) {
-       height: 300px;
-       object-fit: contain;
-    }
-`
+  width: 400px;
+  height: 410px;
+  border-radius: 10px;
+  object-fit: cover;
+  @media screen and (max-width: 425px) {
+    height: 300px;
+    object-fit: contain;
+  }
+`;
 const Icons = styled.div`
-    margin-left: 10px;
-    :hover{
-        color: #3435A8;
-    }
-`
+  margin-left: 10px;
+  :hover {
+    color: #3435a8;
+  }
+`;
 
 const G = styled.div`
-    display: flex;
-    color: #00A85A;
-    margin-top: 10px;
-    font-size: 18px;
-    cursor: pointer;
-    /* :hover{
+  display: flex;
+  color: #00a85a;
+  margin-top: 10px;
+  font-size: 18px;
+  cursor: pointer;
+  /* :hover{
         color: #FBAF1B;
     } */
-`
+`;
 
 const P = styled.div`
-    p{
-        font-size: 1.125rem;
+  p {
+    font-size: 1.125rem;
     line-height: 1.9rem;
     font-weight: 500;
     /* margin-top: 1rem; */
     /* width: 400px; */
     color: #132128;
-    }
-    font-weight: 500;
-    font-size: 1.125rem;
-    line-height: 1.75rem;
-    color: #1e2027;
-    margin-top: 30px;
-    @media screen and (max-width: 425px) {
-        margin-top: -30px;
-    }
-`
+  }
+  font-weight: 500;
+  font-size: 1.125rem;
+  line-height: 1.75rem;
+  color: #1e2027;
+  margin-top: 30px;
+  @media screen and (max-width: 425px) {
+    margin-top: -30px;
+  }
+`;
 
 const Business = styled.div`
-    font-size: 1.875rem;
-    line-height: 2.25rem;
-    font-weight: 500;
-    margin-top: 30px;
-`
+  font-size: 1.875rem;
+  line-height: 2.25rem;
+  font-weight: 500;
+  margin-top: 30px;
+`;
 
 const Icon = styled.img`
-    height: 70px;
-    margin-left: -410px;
-    @media screen and (max-width: 425px) {
-        display: none;
-    }
-`
+  height: 70px;
+  margin-left: -410px;
+  @media screen and (max-width: 425px) {
+    display: none;
+  }
+`;
 
 const Left = styled.div`
-    width: 40%;
-    flex-direction: column;
-    display: flex;
-    @media screen and (max-width: 1024px) {
-        width: 100%;
-        /* background-color: red; */
-        margin-top: 160px;
-    }
-    @media screen and (max-width: 425px) {
-        width: 100%;
-        margin-top: 250px;
-    }
-`
-const Right = styled.div`
-    position: relative;
-    width: 50%;
+  width: 40%;
+  flex-direction: column;
+  display: flex;
+  @media screen and (max-width: 1024px) {
+    width: 100%;
     /* background-color: red; */
-    display: flex;
-    justify-content: center;
-    @media screen and (max-width: 1024px) {
-        width: 100%;
-        display: flex;
-        justify-content: center;
-        align-items: center;
-    }
-`
-
-const Wrapper = styled.div`
-    width: 91%;
-    display: flex;
-    flex-wrap: wrap;
-`
-
-const Container = styled.div`
+    margin-top: 160px;
+  }
+  @media screen and (max-width: 425px) {
+    width: 100%;
+    margin-top: 250px;
+  }
+`;
+const Right = styled.div`
+  position: relative;
+  width: 50%;
+  /* background-color: red; */
+  display: flex;
+  justify-content: center;
+  @media screen and (max-width: 1024px) {
     width: 100%;
     display: flex;
     justify-content: center;
     align-items: center;
-    /* margin-top: 140px; */
-    padding-bottom: 170px;
-    flex-direction: column;
-`
+  }
+`;
+
+const Wrapper = styled.div`
+  width: 91%;
+  display: flex;
+  flex-wrap: wrap;
+`;
+
+const Container = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  /* margin-top: 140px; */
+  padding-bottom: 170px;
+  flex-direction: column;
+`;
--- myapp/src/Components/Join/Join.tsx
+++ myapp/src/Components/Join/Join.tsx
@@ -1,141 +1,148 @@
-import React, {useState} from 'react'
-import styled from 'styled-components'
-import {AiFillPlayCircle} from "react-icons/ai"
-import {BiXCircle} from "react-icons/bi"
+import React, { useState } from "react";
+import styled from "styled-components";
+import { AiFillPlayCircle } from "react-icons/ai";
+import { BiXCircle } from "react-icons/bi";
 
 const Join = () => {
-    const [vid, setVid] = useState<Boolean>(true)
-    const [cancle, setCancle] = useState<Boolean>(true)
+  const [vid, setVid] = useState<Boolean>(true);
+  const [cancle, setCancle] = useState<Boolean>(true);
 
-    const Show = () => {
-        setVid(false)
-    }
+  const Show = () => {
+    setVid(false);
+  };
 
-    const Can = () => {
-        setCancle(true)
-        setVid(true)
-    }
+  const Can = () => {
+    setCancle(true);
+    setVid(true);
+  };
   return (
     <Container>
-        <Top><h1>Watch those that achieve the impossible every day</h1></Top>
-        <Watch onClick={Show}><Icons><AiFillPlayCircle /></Icons>Watch our success story</Watch>
-        {!vid ? (
-            <VidHold>
-              <Card>
-                <iframe 
-                width="853" 
-                height="480" 
-                src="https://www.youtube.com/embed/oCLPj1cXQdI" 
-                title="YouTube video player" 
-                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture">
-            </iframe>
-              </Card>
-              <Cancle onClick={Can} onChangeCapture={Show}><BiXCircle /></Cancle>
+      <Top>
+        <h1>Watch those that achieve the impossible every day</h1>
+      </Top>
+      <Watch onClick={Show}>
+        <Icons>
+          <AiFillPlayCircle />
+        </Icons>
+        Watch our success story
+      </Watch>
+      {!vid ? (
+        <VidHold>
+          <Card>
+            <iframe
+              width="853"
+              height="480"
+              src="https://www.youtube.com/embed/oCLPj1cXQdI"
+              title="YouTube video player"
+              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+            ></iframe>
+          </Card>
+          <Cancle onClick={Can} onChangeCapture={Show}>
+            <BiXCircle />
+          </Cancle>
         </VidHold>
-        ) : null}
+      ) : null}
     </Container>
-  )
-}
+  );
+};
 
-export default Join
+export default Join;
 const Cancle = styled.div`
-    font-size: 40px;
-    position: absolute;
-    right: 20px;
-    top: 90px;
-    color: white;
-    cursor: pointer;
-`
+  font-size: 40px;
+  position: absolute;
+  right: 20px;
+  top: 90px;
+  color: white;
+  cursor: pointer;
+`;
 const Card = styled.div`
-    width: 60%;
-    height: 50%;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-`
+  width: 60%;
+  height: 50%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
 const VidHold = styled.div`
-    width: 100%;
-    height: 100vh;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    background: rgba(0, 0, 0, 0.7);
-    position: absolute;
-    top: 0;
-    position: fixed;
-    z-index: 5;
-`
+  width: 100%;
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background: rgba(0, 0, 0, 0.7);
+  position: absolute;
+  top: 0;
+  position: fixed;
+  z-index: 5;
+`;
 const Wrapper = styled.div`
-    width: 90%;
-    height: 60px;
-    display: flex;
-    margin-top: 70px;
-`
+  width: 90%;
+  height: 60px;
+  display: flex;
+  margin-top: 70px;
+`;
 const Images3 = styled.img`
-    height: 90px;
-    margin-top: -25px;
-`
+  height: 90px;
+  margin-top: -25px;
+`;
 const Images2 = styled.img`
-    height: 100px;
-    margin-top: -25px;
-`
+  height: 100px;
+  margin-top: -25px;
+`;
 
-const Images = styled.img`
-    
-`
+const Images = styled.img``;
 
 const Logo = styled.div`
-    width: 100%;
-    display: flex;
-    justify-content: space-between;
-`
+  width: 100%;
+  display: flex;
+  justify-content: space-between;
+`;
 
 const Icons = styled.div`
-    color: #219653;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    font-size: 25px;
-    margin-right: 10px;
-    cursor: pointer;
-    :hover{
-        visibility: hidden;
-    }
-`
+  color: #219653;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-size: 25px;
+  margin-right: 10px;
+  cursor: pointer;
+  :hover {
+    visibility: hidden;
+  }
+`;
 
 const Watch = styled.div`
-    color: #219653;
-    display: flex;
-    cursor: pointer
-`
+  color: #219653;
+  display: flex;
+  cursor: pointer;
+`;
 
 const Top = styled.h4`
-    h1{
-        color: #219653;
-        font-size: 32px;
-        margin-top: 0px;
-         @media screen and (max-width: 425px) {
-            font-size: 20px;
-            text-align: center;
-        }
-        @media screen and (max-width: 425px) {
-            font-size: 20px;
-        }
-    }
-    font-size: 1.25rem;
-    line-height: 1.2;
+  h1 {
     color: #219653;
-    letter-spacing: .3px;
-`
+    font-size: 32px;
+    margin-top: 0px;
+    @media screen and (max-width: 425px) {
+      font-size: 20px;
+      text-align: center;
+    }
+    @media screen and (max-width: 425px) {
+      font-size: 20px;
+    }
+  }
+  font-size: 1.25rem;
+  line-height: 1.2;
+  color: #219653;
+  letter-spacing: 0.3px;
+`;
 
 const Container = styled.div`
-    width: 100%;
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    background-color: #EFF5FF;
-    align-items: center;
-    padding-bottom: 60px;
-    position: relative;
-    z-index: 2363;
-`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  background-color: #eff5ff;
+  align-items: center;
+  padding-bottom: 60px;
+  position: relative;
+  z-index: 2363;
+`;
