f02e7d3e532488d3938fca447a4883ad098ec396
Damilola Sanni <sannifortune11@gmail.com>
2023-06-26 14:45:22 +0100

    update

---
+++ src/Components/Apis/ApiCalls.tsx
@@ -0,0 +1,59 @@
+import axios from "axios";
+import { AdminData } from "./Allinterfaces";
+
+export const url = "https://cur-uni-abuja.onrender.com/app";
+
+export const createAdmin = async (data: any) => {
+  return await axios
+    .post(`https://cur-uni-abuja.onrender.com/app/register`, data)
+    .then((res) => res.data)
+    .catch((error) => error);
+};
+
+//login Admin
+export const LoginAdmin = async (data: AdminData) => {
+  return await axios
+    .post(`${url}/login`, data)
+    .then((res) => res.data)
+    .catch((res) => res);
+};
+
+export const ReadData = async () => {
+  return await axios
+    .get("https://cur-uni-abuja.onrender.com/app/image/getall")
+    .then((res) => res.data)
+    .catch((err) => err);
+};
+
+export const ReadImage = async () => {
+  return await axios
+    .get("https://cur-uni-abuja.onrender.com/app/gallery/getall")
+    .then((res) => res.data)
+    .catch((err) => err);
+};
+
+export const postText = async (data: any) => {
+  return await axios
+    .post("https://cur-uni-abuja.onrender.com/app/text/post")
+    .then((res) => res.data)
+    .catch((error) => error);
+};
+
+export const updateText = async ({ writeups, button }: any) => {
+  return await axios
+    .patch(
+      "https://cur-uni-abuja.onrender.com/app/text/update/6499886d8728214477f96989",
+      {
+        writeups,
+        button,
+      }
+    )
+    .then((res) => res.data)
+    .catch((error) => error);
+};
+export const ReadText = async () => {
+  return await axios
+    .get(`https://cur-uni-abuja.onrender.com/app/text/gettext`)
+    .then((res) => res.data)
+    .catch((error) => error);
+};
---
+++ src/Components/UploadPage/UploadText.tsx
@@ -0,0 +1,212 @@
+import React, { useState } from "react";
+import styled from "styled-components";
+import axios from "axios";
+import Loading from "./Loading";
+import Adminhead from "../Adminhead/Adminhead";
+import Footer from "../Footer/Footer";
+// import Swal from "sweetalert2";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { updateText } from "../Apis/ApiCalls";
+
+const UploadText = () => {
+  const [show, setShow] = React.useState(false);
+  const [writeups, setWriteups] = React.useState<any>("");
+  const [button, setButton] = React.useState<any>("");
+
+  const queryClient = useQueryClient();
+  const updateTextFn = useMutation({
+    mutationFn: (data: any) => updateText(data),
+    onSuccess: (data) => {
+      queryClient.invalidateQueries(["alltext"]);
+    },
+  });
+
+  const handleSubmit = () => {
+    updateTextFn.mutate({
+      writeups,
+      button,
+    });
+  };
+
+  return (
+    <Container>
+      <Adminhead />
+      {show ? <Loading /> : null}
+      <Wrapper>
+        <Card>
+          <Inp
+            onChange={(e) => {
+              setWriteups(e.target.value);
+            }}
+            placeholder="Enter Text"
+          />
+
+          <Inp
+            onChange={(e) => {
+              setButton(e.target.value);
+            }}
+            placeholder="Enter Summary"
+          />
+
+          {writeups !== "" &&
+          // category !== "" &&
+          button !== "" ? (
+            <Button2 onClick={handleSubmit} cp="pointer" bg="black">
+              {updateTextFn.isLoading ? "Loading.." : "Submit"}
+            </Button2>
+          ) : (
+            <Button2 cp="not-allowed" bg="silver">
+              Submit
+            </Button2>
+          )}
+          {/* <Button2
+            onClick={handleSubmit}
+            cp={writeups !== "" && button !== "" ? "pointer" : "not-allowed"}
+            bg={writeups !== "" && button !== "" ? "black" : "silver"}
+          >
+            Submit
+          </Button2> */}
+        </Card>
+      </Wrapper>
+      <br />
+      <br />
+      <Footer />
+    </Container>
+  );
+};
+
+export default UploadText;
+
+const Img = styled.img`
+  height: 100%;
+  width: 100%;
+  object-fit: cover;
+`;
+
+const Button2 = styled.button<{ bg: string; cp: string }>`
+  height: 40px;
+  width: 150px;
+  background-color: ${(props) => props.bg};
+  color: white;
+  border: none;
+  outline: none;
+  border-radius: 5px;
+  transition: all 350ms;
+  cursor: ${(props) => props.cp};
+  margin-top: 10px;
+  :hover {
+    transform: scale(0.95);
+  }
+  @media screen and (max-width: 500px) {
+    width: 120px;
+  }
+`;
+
+const Select = styled.select`
+  width: 370px;
+  height: 30px;
+  border-radius: 3px;
+  border: 0.7px solid silver;
+  outline-color: #f8cbe0;
+  padding-left: 10px;
+`;
+
+const TexArea = styled.textarea`
+  width: 350px;
+  height: 70px;
+  border-radius: 3px;
+  border: 0.7px solid silver;
+  outline-color: #f8cbe0;
+  padding-left: 10px;
+  resize: none;
+`;
+
+const Inp = styled.input`
+  margin: 10px;
+  width: 350px;
+  height: 30px;
+  border-radius: 3px;
+  border: 0.7px solid silver;
+  outline-color: #f8cbe0;
+  padding-left: 10px;
+  @media screen and (max-width: 425px) {
+    width: 300px;
+  }
+  @media screen and (max-width: 375px) {
+    width: 250px;
+  }
+`;
+
+const Input = styled.input`
+  display: none;
+`;
+const Button = styled.label`
+  height: 40px;
+  width: 200px;
+  background-color: #00a85a;
+  color: white;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  border-radius: 5px;
+  transition: all 350ms;
+  margin-top: 10px;
+  cursor: pointer;
+  :hover {
+    transform: scale(0.98);
+  }
+`;
+
+const Circle = styled.div`
+  height: 150px;
+  width: 250px;
+  border-radius: 5px;
+  /* border-radius: 50%; */
+  background-color: #e6e4e4;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  color: gray;
+  flex-direction: column;
+  font-size: 30px;
+  span {
+    font-size: 12px;
+  }
+`;
+
+const Card = styled.div`
+  box-shadow: rgba(0, 0, 0, 0.02) 0px 1px 3px 0px,
+    rgba(27, 31, 35, 0.15) 0px 0px 0px 1px;
+  width: 600px;
+  /* height: 300px; */
+  margin-top: 100px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  /* padding-top: 10px;
+	padding-bottom: 10px; */
+  /* background-color: red; */
+  padding: 25px;
+  @media screen and (max-width: 425px) {
+    width: 350px;
+    padding-right: 10px;
+    padding-left: 10px;
+    margin-top: 40px;
+  }
+  @media screen and (max-width: 375px) {
+    width: 300px;
+    padding-right: 10px;
+    padding-left: 10px;
+    margin-top: 30px;
+  }
+`;
+
+const Wrapper = styled.div``;
+
+const Container = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+`;
